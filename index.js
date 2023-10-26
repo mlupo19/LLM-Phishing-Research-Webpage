07839c5d33afa3e9f4fdd178c036128490122f8b
= <=>
2023-10-26 14:48:56 -0400

    Add content

---
+++ index.js
@@ -0,0 +1,17 @@
+// Function to get URL parameters
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
+// Usage example
+var id = getParameterByName('id');
+
+// Use the extracted parameters as needed
+console.log("id: " + id);
